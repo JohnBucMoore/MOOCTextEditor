4756e21b21bffc645a684e45c539809b505e2c95
John Buchanan Moore <johnbucmoore@gmail.com>
2020-01-11 14:39:09 -0500

    add ImprovedDocument class for later regex improvement

---
+++ MOOCTextEditor/src/document/ImprovedDocument.java
@@ -0,0 +1,9 @@
+package document;
+
+public class ImprovedDocument {
+	//Our REGEX are pretty naive. For example, the word 7.5 causes 
+	//real problems for our expressions as a “.” usually denotes 
+	//the end of a sentence. If you want to learn more about REGEX, 
+	//you can improve upon our approach. Make sure you don't use 
+	//it to produce your grading output.
+}
